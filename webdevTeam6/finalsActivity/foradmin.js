49837af94d686b098367200315e8bf166b77c1b0
Paoxyz <paobutdiff@gmail.com>
2025-04-10 07:26:18 +0800

    sa admin log in to

---
+++ webdevTeam6/finalsActivity/foradmin.js
@@ -0,0 +1,22 @@
+
+  const correctUser = "admin";
+  const correctPass = "admin123";
+  const correctPin = "1234";
+
+  function login() {
+         // Dummy login
+         window.location.href = "menu.html"; 
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    const pin = document.getElementById("pin").value;
+    const error = document.getElementById("error");
+
+    if (user === correctUser && pass === correctPass && pin === correctPin) {
+      // Redirect to dashboard
+      window.location.href = "menu.html";
+    } else {
+      error.textContent = "Incorrect credentials or PIN.";
+    }
+    
+  }
+
